511856ad2eb542b558462f288dbdf16d831d14a8
Adam <you@example.com>
2021-11-18 13:07:56 -0800

    white space fixes

--- bootstrap/dump.asm
+++ bootstrap/dump.asm
@@ -11,15 +11,15 @@
     fcb $FF ;68HC11 wants the first byte to be 0xFF
 
 Start:
-    ldS #$FF            ; stack pointer = 0xFF
-    ldX #$1000          ; config 68HC11 registers
-    ldD #$330C          ; config SCI
+    ldS     #$FF        ; stack pointer = 0xFF
+    ldX     #$1000      ; config 68HC11 registers
+    ldD     #$330C      ; config SCI
     staA    $2B,X       ; store value in register A ($33) in 0x1000 + 0x002B
                         ; configures SCI for 1200 BAUD
     staB    $2D,X       ; store value in register B ($0C) in 0x1000 + 0x002D
                         ; configures SCI for TXD/RXD
     sei                 ; disable interupts
-    ldD #$6001          ; load D register with 0x6001
+    ldD     #$6001          ; load D register with 0x6001
     staA    $3C,X       ; store value in register A ($60) in 0x1000 + 0x003C
                         ; enables Special Mode
     staB    $3F,X       ; store valie in register B ($01) in 0x1000 + 0x003F
@@ -38,7 +38,7 @@ Start:
     bra     SendByte
 
 ThirtyTwoK:
-    ldX #$8000          ; load the start address in register X
+    ldX     #$8000       ; load the start address in register X
 
 SendByte:
 

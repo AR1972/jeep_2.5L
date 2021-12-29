0317d7bfc3dc2907ac1dba2f8e5ba9a5909885c7
Adam <you@example.com>
2021-12-29 14:41:11 -0800

    add bootstrap template

---
+++ bootstrap/template.asm
@@ -0,0 +1,40 @@
+; 68HC11 bootstrap, download to a SBEC that has been
+; placed into bootstrap mode by applying 12 volts to
+; pin 45 until SBEC sends 0x00 on pin 25.
+
+    ORG $0000
+
+THE_BEGIN:
+Start:
+    ldS     #$FF        ; stack pointer = 0xFF
+    ldX     #$1000      ; config 68HC11 registers
+    ldD     #$330C      ; config SCI
+    staA    $2B,X       ; store value in register A ($33) in 0x1000 + 0x002B
+                        ; configures SCI for 1200 BAUD
+    staB    $2D,X       ; store value in register B ($0C) in 0x1000 + 0x002D
+                        ; configures SCI for TXD/RXD
+    sei                 ; disable interrupts
+    ldD     #$6001      ; load D register with 0x6001
+    staA    $3C,X       ; store value in register A ($60) in 0x1000 + 0x003C
+                        ; enables Special Mode
+    staB    $3F,X       ; store value in register B ($01) in 0x1000 + 0x003F
+                        ; System Config Register
+
+; give receiver around 100ms
+
+    ldX     #$7E00
+Delay:
+    deX
+    bne     Delay
+
+; <----------[ code here ]---------->
+
+Done:
+    stop
+
+THE_END:
+
+                        ; pad bootstrap to 256 total bytes
+ REPEAT 256-(THE_END-THE_BEGIN)
+    fcb 0x00
+ ENDR

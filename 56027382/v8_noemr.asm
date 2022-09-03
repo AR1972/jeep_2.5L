878b147ab685cb6a4b7e603a8327b4f96a94d9e0
Adam <you@example.com>
2022-09-02 18:42:30 -0700

    build eprom image intended to be cross flashed to a 5.2L ECU, (E)mission (M)aintenance (R)eminder code has been disabled

---
+++ 56027382/v8_noemr.asm
@@ -0,0 +1,14 @@
+FILCHR TEXT $3F
+ OPT NOW
+ OPT H11
+ ORG 0
+
+Data_start equ $2000
+Code_start equ $4000
+BaseAddr equ $0400
+NOEMR equ 1
+ include ..\common\defines.inc
+ ORG Data_start
+ include data.inc
+ ORG Code_start
+ include code.inc
